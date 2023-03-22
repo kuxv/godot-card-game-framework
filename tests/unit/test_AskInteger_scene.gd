4c9da86a2092a9c58c490829d45e52e54e3ee0f1
kuxv <kuxv@saloh.org>
2023-03-22 22:31:51 +0100

    test AskInteger - fix await

--- tests/unit/test_AskInteger_scene.gd
+++ tests/unit/test_AskInteger_scene.gd
@@ -15,9 +15,7 @@ func after_all():
 	cfc.game_settings.fancy_movement = true
 
 func before_each():
-	var confirm_return = setup_board()
-	if confirm_return is GDScriptFunctionState: # Still working.
-		confirm_return = await confirm_return.completed
+	var confirm_return = await setup_board()
 	ask_integer = ScriptingEngine._ASK_INTEGER_SCENE.instantiate()
 	hh = ask_integer.get_node("HorizontalHighlights")
 	vh = ask_integer.get_node("VecticalHighlights")
